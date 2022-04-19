1c049b6eca04a98f936cd6ae33aa0d3ab00a4cfa
Sanjay Rawat <sanjay.rawat@saffrontech.net>
2022-04-19 19:00:41 +0530

    Feat : basic redux implemention

--- basic/index.js
+++ basic/index.js
@@ -1,1 +1,33 @@
-console.log("hi there")
+const redux = require('redux')
+const createStore = redux.createStore;
+
+const BUY_ITEM = 'BUY_ITEM'
+const buy = () => {
+    return {
+        type: BUY_ITEM
+    }
+}
+const INITIAL_STATE = {
+    no_of_item: 20
+}
+
+const reducer = ((state = INITIAL_STATE, action) => {
+    switch (action.type) {
+        case BUY_ITEM:
+            return {
+                ...state,
+                no_of_item: state.no_of_item - 1
+            }
+        default:
+            return { ...state }
+    }
+})
+
+const store = createStore(reducer);
+const unsubscribe= store.subscribe(() => {
+    console.log(`Updated state : `, store.getState())
+})
+store.dispatch(buy()); 
+store.dispatch(buy()); 
+store.dispatch(buy());
+unsubscribe();
